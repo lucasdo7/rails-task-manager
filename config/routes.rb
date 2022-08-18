30150683aaccb561932b9f042e0e9bb502fac742
lucasmendesdias <lucasbiocelp@gmail.com>
2022-08-18 09:19:19 -0300

    falta desbugar o before action

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,28 @@ Rails.application.routes.draw do
 
 
   # verb "path", to: "controller#action"
+
+  # homepage
   root to: 'tasks#home'
+
+  # show all
   get 'tasks', to: 'tasks#index'
-  get 'tasks/:id', to: 'tasks#show'
+
+  # create a task
+  # .1 create a form
+  get 'tasks/new', to: 'tasks#new', as: 'task_new'
+  # .2 send the form
+  post 'tasks', to: 'tasks#create'
+
+  # show 1
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # edit task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
 
 end
